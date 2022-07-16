dca0a4ac66a6ac5b3de349aae8a69d814a85417d
conceicaoRibeiro <carolmoons.cs@gmail.com>
2022-07-16 20:56:50 -0300

    Api test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SalesRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmeta.entities.Sales;
+
+public interface SalesRepository extends JpaRepository<Sales, Long> {
+
+}
